915247b7da370cf9b786dae7c586556a67d9b779
Felipe Teixeira Schefer <felipe_official@outlook.com>
2021-12-27 23:32:57 -0300

    handlebar

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -1,11 +1,12 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('handlebars', expressHbs() )
+app.set('view engine', 'handlebars');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -18,7 +19,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page not Found'})
 });
 
 app.listen(3000);
--- EXPRESS/routes/admin.js
+++ EXPRESS/routes/admin.js
@@ -1,16 +1,13 @@
 const path = require('path');
-
 const express = require('express');
-
 const rootDir = require('../util/path');
-
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle:'Add Product', path: '/admin/add-product'})
 });
 
 // /admin/add-product => POST
