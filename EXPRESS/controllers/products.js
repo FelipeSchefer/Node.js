3859ffb3331fa628afcb12ce3b84a18530130eb5
Felipe Teixeira Schefer <felipe_official@outlook.com>
2022-01-06 20:53:19 -0300

    Storing Data in Files via the Model and Fetching Data files via the Model

--- EXPRESS/controllers/products.js
+++ EXPRESS/controllers/products.js
@@ -1,6 +1,5 @@
 const Product = require('../models/product')
 
-
 exports.getAddProducts =  (req, res, next) => {
  res.render('add-product', {
    pageTitle: 'Add Product',
@@ -18,13 +17,14 @@ exports.postAddProducts = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll()
-  res.render('shop', {
-   prods: products,
-   pageTitle: 'Shop',
-   path: '/',
-   hasProducts: products.length > 0,
-   activeShop: true,
-   productCSS: true
- });
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  })
 }
--- EXPRESS/models/product.js
+++ EXPRESS/models/product.js
@@ -1,14 +1,41 @@
-const products = []
+const fs = require('fs')
+// const { dirname } = require('path')
+const path = require('path')
+
 module.exports = class Product {
  constructor(t){
   this.title = t
  }
 
  save(){
-  products.push(this)
+  const p = path.join(
+   path.dirname(process.mainModule.filename),
+   'data',
+   'products.json'
+  )
+  fs.readFile(p, (err, fileContent) => {
+   let products = []
+   if (!err){
+    products = JSON.parse(fileContent)
+   }
+   products.push(this)
+   fs.writeFile(p, JSON.stringify(products), (err) => {
+    console.log(err) 
+   })
+  })
  }
 
- static fetchAll(){
-  return products
+ static fetchAll(callback){
+  const p = path.join(
+   path.dirname(process.mainModule.filename),
+   'data',
+   'products.json'
+  )
+  fs.readFile(p, (err, fileContent)=>{
+   if(err){
+    callback([])
+   }
+   callback(JSON.parse(fileContent))
+  })
  }
 }
